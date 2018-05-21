5d40c0066452056395539efea8b24c2e11d99b39
Liugan <liugan1996@foxmail.com>
2018-05-21 12:16:20 +0800

    add: 登陆注册邮箱激活的数据库操作层接口

--- src/main/java/com/gsafety/repository/UserRepositoty.java
+++ src/main/java/com/gsafety/repository/UserRepositoty.java
@@ -7,19 +7,39 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
- *
  * @author Liugan
  * @date 2017/2/15
  * User表操作接口
  */
 @Repository
-public interface UserRepositoty extends JpaRepository<User,Long>{
+public interface UserRepositoty extends JpaRepository<User, String> {
 
-    /**
-     * 根据姓名查找用户
-     * @param name
-     * @return
-     */
-    @Query("select t from User t where t.name = :name")
-    User findByUserName(@Param("name") String name);
+	/**
+	 * 根据邮箱查找用户
+	 *
+	 * @param userEmail 用户邮箱
+	 * @return 用户对象
+	 */
+	@Query("select t from User t where t.userEmail = :userEmail")
+	User findByUserEmail(@Param("userEmail") String userEmail);
+
+	/**
+	 * 根据姓名查找用户
+	 *
+	 * @param userName 用户姓名
+	 * @return 用户对象
+	 */
+	@Query("select t from User t where t.userName = :userName")
+	User findByUserName(@Param("userName") String userName);
+
+	/**
+	 * 根据id查找用户
+	 *
+	 * @param userId 用户Id
+	 * @return 用户对象
+	 */
+	@Query("select t from User t where t.userId = :userId")
+	User findByUserId(@Param("userId") String userId);
+
+
 }
